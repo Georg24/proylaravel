9258d8974839ca9eb1f097aec3177adc2b2d6336
Georg24 <jlmedina2405@gmail.com>
2017-05-06 09:36:02 -0400

    instalacion de bootstrap y form html de laravel collective

---
+++ resources/lang/en/login.php
@@ -0,0 +1,6 @@
+<?php
+return [
+  'user' => 'user',
+  'password' => 'password',
+  'role' => 'role'
+];
---
+++ resources/lang/es/login.php
@@ -0,0 +1,6 @@
+<?php
+return [
+  'user' => 'usuario',
+  'password' => 'contraseña',
+  'role' => 'rol'
+];
